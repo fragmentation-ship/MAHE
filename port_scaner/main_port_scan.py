5a141f6d14721ea78264538fdcbf4f04ee7b215a
fragmentation-ship <youhavenopass@gmail.com>
2017-12-22 00:21:55 +0200

    Update main_port_scan.py
    Added code for port-scanner.

--- port_scaner/main_port_scan.py
+++ port_scaner/main_port_scan.py
@@ -1,1 +1,21 @@
 import socket
+
+host = input("Enter a host for scan: ")
+scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def primary_scan(port):
+  try:
+    scanner.connect((host, port))
+  except:
+    return False
+  
+for array_scan in range(1, 65535):
+  if primary_scan(array_scan):
+    print("Port:", array_scan, "is open!")
+  else:
+    print("Port:", array_scan, "is close!")
+
+for x in range(1, 1024):
+  t = threading.Thread(target=primary_scan)
+  t.start()
+  
